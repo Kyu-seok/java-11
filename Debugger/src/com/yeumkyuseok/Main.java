e0e972d7201dddfc2c369904936e753d5ab59987
yeumkyuseok <yeumkyuseok@gmail.com>
2021-08-12 23:35:51 +0900

    update on Debugger

--- Debugger/src/com/yeumkyuseok/Main.java
+++ Debugger/src/com/yeumkyuseok/Main.java
@@ -3,5 +3,11 @@ package com.yeumkyuseok;
 public class Main {
 
     public static void main(String[] args) {
+        StringUtilities utils = new StringUtilities();
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < 10) {
+            utils.addChar(sb, 'a');
+        }
+        System.out.println(sb);
     }
 }
