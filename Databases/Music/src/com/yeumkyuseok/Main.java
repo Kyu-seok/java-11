d86d1dec8724ee7c9c03b4a4b8b63f5bf7e5d0f3
yeumkyuseok <yeumkyuseok@gmail.com>
2021-08-14 16:59:49 +0900

    static final variables declared

--- Databases/Music/src/com/yeumkyuseok/Main.java
+++ Databases/Music/src/com/yeumkyuseok/Main.java
@@ -1,8 +1,35 @@
 package com.yeumkyuseok;
 
+import com.yeumkyuseok.model.Artist;
+import com.yeumkyuseok.model.Datasource;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Datasource datasource = new Datasource();;
+        if (!datasource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        List<Artist> artists = datasource.queryArtists(Datasource.ORDER_BY_ASC);
+        if (artists == null) {
+            System.out.println("No artists!");
+            return;
+        }
+
+        for (Artist artist : artists) {
+            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
+        }
+
+        List<String> albumsForArtist = datasource.queryAlbumsForArtist("Pink Floyd", Datasource.ORDER_BY_DESC);
+
+        for (String album : albumsForArtist) {
+            System.out.println(album);
+        }
+
+        datasource.close();
     }
 }
