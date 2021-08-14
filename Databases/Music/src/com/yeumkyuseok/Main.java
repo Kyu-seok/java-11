189511cffa71bd76013f3e7709086be8ad50397f
yeumkyuseok <yeumkyuseok@gmail.com>
2021-08-15 03:09:19 +0900

    query functionality added

--- Databases/Music/src/com/yeumkyuseok/Main.java
+++ Databases/Music/src/com/yeumkyuseok/Main.java
@@ -2,7 +2,9 @@ package com.yeumkyuseok;
 
 import com.yeumkyuseok.model.Artist;
 import com.yeumkyuseok.model.Datasource;
+import com.yeumkyuseok.model.SongArtist;
 
+import javax.xml.crypto.Data;
 import java.util.List;
 
 public class Main {
@@ -29,6 +31,25 @@ public class Main {
         for (String album : albumsForArtist) {
             System.out.println(album);
         }
+
+        List<SongArtist> songArtists = datasource.qureyArtistsForSong("Go Your Own Way", Datasource.ORDER_BY_ASC);
+        if (songArtists == null) {
+            System.out.println("Couldn't find the artist ofor the song");
+            return;
+        }
+
+        for (SongArtist artist : songArtists) {
+            System.out.println("Artist name = " + artist.getArtistName() +
+                    " Album name = " + artist.getAlbumName() +
+                    " Track = " + artist.getTrack());
+        }
+
+        datasource.querySongsMetadata();
+
+        int count = datasource.getCount(Datasource.TABLE_SONGS);
+        System.out.println("Number of songs is: " + count);
+
+        datasource.createViewForSongArtists();
 
         datasource.close();
     }
