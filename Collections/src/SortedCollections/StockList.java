ab29f055a214e382e85a0b93fc2bf6e947e67eda
yeumkyuseok <yeumkyuseok@gmail.com>
2021-11-01 06:46:27 +0900

    functionality added

--- Collections/src/SortedCollections/StockList.java
+++ Collections/src/SortedCollections/StockList.java
@@ -1,5 +1,25 @@
 package SortedCollections;
 
-public class StockList {
+import java.util.HashMap;
+import java.util.Map;
 
+public class StockList {
+    private final Map<String, StockItem> list;
+
+    public StockList() {
+        this.list = new HashMap<>();
+    }
+
+    public int addStock(StockItem item) {
+        if (item != null) {
+            // check if already have quantities of this item
+            StockItem inStock = list.getOrDefault(item.getName(), item);
+            // If there are already stocks on this item, adjust the quantity
+            if (inStock != item) {
+                item.adjustStock(inStock.getQuantityStock());
+            }
+
+            list.put(item.getName(), item);
+        }
+    }
 }
