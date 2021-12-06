19fd75f3fee46a17ea8b3283d97554728781d706
yeumkyuseok <yeumkyuseok@gmail.com>
2021-12-07 01:08:58 +0900

    Implemented two different ways of catching exceptions, EAFP VS LBYL

--- Java_IO/Exceptions/src/com/yeumkyuseok/Main.java
+++ Java_IO/Exceptions/src/com/yeumkyuseok/Main.java
@@ -1,12 +1,52 @@
 package com.yeumkyuseok;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int x = 98;
-        int y = 0;
-        System.out.println(divideLBYL(x, y));
-        System.out.println(divideEAFP(x, y));
+//        int x = 98;
+//        int y = 0;
+//        System.out.println(divideLBYL(x, y));
+//        System.out.println(divideEAFP(x, y));
+//        divide(x, y);
+//        int x = getIntLBYL();
+        int x = getIntEAFP();
+        System.out.println("x is " + x);
+    }
+
+    private static int getInt() {
+        Scanner s = new Scanner(System.in);
+        return s.nextInt();
+    }
+
+    private static int getIntLBYL() {
+        Scanner s = new Scanner(System.in);
+        boolean isValid = true;
+        System.out.println("Please enter an integer ");
+        String input = s.next();
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid) {
+            return Integer.parseInt(input);
+        }
+
+        return 0;
+    }
+
+    private static int getIntEAFP() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter an integer ");
+        try {
+            return s.nextInt();
+        } catch (InputMismatchException e) {
+            return 0;
+        }
     }
 
     private static int divideLBYL(int x, int y) {
@@ -23,5 +63,9 @@ public class Main {
         } catch (ArithmeticException e) {
             return 0;
         }
+    }
+
+    private static int divide(int x, int y) {
+        return x / y;
     }
 }
