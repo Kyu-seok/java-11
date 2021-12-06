fc79b663677773b8fce9ed2d46b853ea53c51b19
yeumkyuseok <yeumkyuseok@gmail.com>
2021-12-07 00:58:42 +0900

    re-initialized Java_IO/Exceptions

--- Java_IO/Exceptions/src/com/yeumkyuseok/Main.java
+++ Java_IO/Exceptions/src/com/yeumkyuseok/Main.java
@@ -1,45 +1,12 @@
 package com.yeumkyuseok;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        int x = getIntEAFP();
-        System.out.println("x is " + x);
-    }
-
-    private static int getInt() {
-        Scanner s = new Scanner(System.in);
-        return s.nextInt();
-    }
-
-    private static int getIntLBYL() {
-        Scanner s = new Scanner(System.in);
-        boolean isValid = true;
-        System.out.println("Please enter an integer");
-        String input = s.next();
-        for (int i = 0; i < input.length() ; i++) {
-            if (!Character.isDigit(input.charAt(i))) {
-                isValid = false;
-                break;
-            }
-        }
-        if (isValid) {
-            return Integer.parseInt(input);
-        }
-        return 0;
-    }
-
-    private static int getIntEAFP() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Please enter an integer ");
-        try {
-            return s.nextInt();
-        } catch (InputMismatchException e) {
-            return 0;
-        }
+        int x = 98;
+        int y = 0;
+        System.out.println(divideLBYL(x, y));
+        System.out.println(divideEAFP(x, y));
     }
 
     private static int divideLBYL(int x, int y) {
@@ -52,13 +19,9 @@ public class Main {
 
     private static int divideEAFP(int x, int y) {
         try {
-            return x / y;
+          return x / y;
         } catch (ArithmeticException e) {
             return 0;
         }
-    }
-
-    private static int divide(int x, int y) {
-        return x / y;
     }
 }
