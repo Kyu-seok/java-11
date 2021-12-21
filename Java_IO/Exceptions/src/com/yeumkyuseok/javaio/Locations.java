578d244afefa63fec75e55bc757eb2cac0f36d3a
yeumkyuseok <yeumkyuseok@gmail.com>
2021-12-21 22:26:37 +0900

    Implemented buffered reader & writer

--- Java_IO/Exceptions/src/com/yeumkyuseok/javaio/Locations.java
+++ Java_IO/Exceptions/src/com/yeumkyuseok/javaio/Locations.java
@@ -27,7 +27,31 @@ public class Locations implements Map<Integer, Location>  {
     }
 
     static {
-        try(Scanner scanner = new Scanner(new BufferedReader(new FileReader("locations_big.txt")))) {
+        try (DataInputStream locFile = new DataInputStream(new BufferedInputStream(new FileInputStream("locations.dat")))) {
+            boolean eof = false;
+            while (!eof) {
+                try {
+                    Map<String, Integer> exits = new LinkedHashMap<>();
+                    int locID = locFile.readInt();
+                    String description = locFile.readUTF();
+                    int numExits = locFile.readInt();
+                    System.out.println("Read location " + locID + " : " + description);
+                    System.out.println("Found " + numExits + " exits");
+                    for (int i = 0; i < numExits; i++) {
+                        String direction = locFile.readUTF();
+                        int destination = locFile.readInt();
+                        exits.put(direction, destination);
+                        System.out.println("\t\t" + direction + "," + destination);
+                    }
+                    locations.put(locID, new Location(locID, description, exits));
+                } catch (EOFException e) {
+                    eof = true;
+                }
+            }
+        } catch (IOException io) {
+            System.out.println("IO Exception");
+        }
+        /*try(Scanner scanner = new Scanner(new BufferedReader(new FileReader("locations_big.txt")))) {
             scanner.useDelimiter(",");
             while (scanner.hasNextLine()) {
                 int loc = scanner.nextInt();
@@ -55,7 +79,7 @@ public class Locations implements Map<Integer, Location>  {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     @Override
