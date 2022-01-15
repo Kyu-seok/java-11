0904f94a5bc5f92c6722b8b9fcbb3f70a685cde0
yeumkyuseok <yeumkyuseok@gmail.com>
2022-01-15 18:20:26 +0900

    Implemented test cases

--- Debugging_&_Unit_Testing/Banking/src/com/yeumkyuseok/BankAccount.java
+++ Debugging_&_Unit_Testing/Banking/src/com/yeumkyuseok/BankAccount.java
@@ -6,28 +6,41 @@ public class BankAccount {
     private String lastName;
     private double balance;
 
-    public BankAccount(String firstName, String lastName, double balance) {
+    public static final int CHECKING = 1;
+    public static final int SAVINGS = 2;
+
+    private int accountType;
+
+    public BankAccount(String firstName, String lastName, double balance, int typeOfAccount) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
+        this.accountType = typeOfAccount;
     }
 
-    // The branch argument is true if the customer is performing the transcation at a branch, with a teller.
+    // The branch argument is true if the customer is performing the transaction at a branch, with a teller.
     // False if customer is performing the transaction at an atm
     public double deposit(double amount, boolean branch) {
         balance += amount;
         return balance;
     }
 
-    // The branch argument is true if the customer is performing the transcation at a branch, with a teller.
+    // The branch argument is true if the customer is performing the transaction at a branch, with a teller.
     // False if customer is performing the transaction at an atm
     public double withdraw(double amount, boolean branch) {
+        if ((amount > 500.00) && !branch) {
+            throw new IllegalArgumentException();
+        }
         balance -= amount;
         return balance;
     }
 
     public double getBalance() {
         return balance;
+    }
+
+    public boolean isChecking() {
+        return accountType == CHECKING;
     }
 
 }
