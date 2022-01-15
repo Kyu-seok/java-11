b0fb99fe4b0341b1bccf684a88e07d1c70801682
yeumkyuseok <yeumkyuseok@gmail.com>
2022-01-15 16:51:42 +0900

    Initialized BankAccount Project

---
+++ Debugging_&_Unit_Testing/Banking/src/com/yeumkyuseok/BankAccountTest.java
@@ -0,0 +1,26 @@
+package com.yeumkyuseok;
+
+// import static org.junit.Assert.*;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.fail;
+
+public class BankAccountTest {
+
+    @org.junit.Test
+    public void deposit() {
+        BankAccount account = new BankAccount("Kyu", "Seok", 1000.00);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
+    }
+
+    @org.junit.Test
+    public void withdraw() {
+        fail("This test has yet to be implemented");
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+        fail("This test has yet to be implemented");
+    }
+
+}
