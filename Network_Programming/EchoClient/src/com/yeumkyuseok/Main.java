dcc81c68972c269ceae5af158ebc246f5852a6c3
yeumkyuseok <yeumkyuseok@gmail.com>
2022-01-18 13:58:58 +0900

    Multi Threading added

--- Network_Programming/EchoClient/src/com/yeumkyuseok/Main.java
+++ Network_Programming/EchoClient/src/com/yeumkyuseok/Main.java
@@ -2,12 +2,14 @@ package com.yeumkyuseok;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         try (Socket socket = new Socket("localhost", 5001)) {
+            socket.setSoTimeout(5000);
             BufferedReader echoes = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
 
@@ -25,7 +27,8 @@ public class Main {
                     System.out.println(response);
                 }
             } while (!echoString.equals("exit"));
-
+        } catch (SocketTimeoutException e) {
+            System.out.println("The socket timed out");
         } catch (IOException e) {
             System.out.println("Client Error: " + e.getMessage());
         }
