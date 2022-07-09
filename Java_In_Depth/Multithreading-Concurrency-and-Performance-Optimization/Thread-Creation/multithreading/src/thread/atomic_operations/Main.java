7ff7c0f739311291a909aa1bf875e4473276d637
Kyu-seok <yeumkyuseok@gmail.com>
2022-07-09 22:33:51 +0900

    Implement Metrics class in Main.java

--- Java_In_Depth/Multithreading-Concurrency-and-Performance-Optimization/Thread-Creation/multithreading/src/thread/atomic_operations/Main.java
+++ Java_In_Depth/Multithreading-Concurrency-and-Performance-Optimization/Thread-Creation/multithreading/src/thread/atomic_operations/Main.java
@@ -2,4 +2,20 @@ package thread.atomic_operations;
 
 public class Main {
 
+    public static class Metrics {
+        private long count = 0;
+        private double average = 0.0;
+
+        public void addSample(long sample) {
+            double currentSum = average * count;
+            count++;
+            average = (currentSum + sample) / count;
+        }
+
+        public double getAverage() {
+            return average;
+        }
+
+    }
+
 }
