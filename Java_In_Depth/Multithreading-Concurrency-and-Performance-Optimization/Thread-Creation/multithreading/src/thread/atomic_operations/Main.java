0656467c00ea897b0e707186bafc86b4db820cde
Kyu-seok <yeumkyuseok@gmail.com>
2022-07-11 22:39:23 +0900

    Implement BusinessLogic class in Main.java

--- Java_In_Depth/Multithreading-Concurrency-and-Performance-Optimization/Thread-Creation/multithreading/src/thread/atomic_operations/Main.java
+++ Java_In_Depth/Multithreading-Concurrency-and-Performance-Optimization/Thread-Creation/multithreading/src/thread/atomic_operations/Main.java
@@ -1,6 +1,35 @@
 package thread.atomic_operations;
 
+import java.util.Random;
+
 public class Main {
+
+    public static class BusinessLogic extends Thread {
+        private Metrics metrics;
+        private Random random = new Random();
+
+        public BusinessLogic(Metrics metrics) {
+            this.metrics = metrics;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long start = System.currentTimeMillis();
+
+                try {
+                    Thread.sleep(random.nextInt(10));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                long end = System.currentTimeMillis();
+                metrics.addSample(end - start);
+            }
+
+        }
+    }
+
 
     public static class Metrics {
         private long count = 0;
