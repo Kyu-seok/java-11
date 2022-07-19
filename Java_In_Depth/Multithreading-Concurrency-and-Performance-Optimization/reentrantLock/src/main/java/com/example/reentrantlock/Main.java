af6a4c4a04a316ab31d72050b5a61e57ddb3b530
Kyu-seok <yeumkyuseok@gmail.com>
2022-07-19 21:51:57 +0800

    Implement JavaFX

--- Java_In_Depth/Multithreading-Concurrency-and-Performance-Optimization/reentrantLock/src/main/java/com/example/reentrantlock/Main.java
+++ Java_In_Depth/Multithreading-Concurrency-and-Performance-Optimization/reentrantLock/src/main/java/com/example/reentrantlock/Main.java
@@ -24,7 +24,87 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Main extends Application{
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    private void addWindowResizeListener(Stage stage, Rectangle background) {
+        ChangeListener<Number> stageSizeListener = ((observable, oldValue, newValue) -> {
+            background.setHeight(stage.getHeight());
+            background.setWidth(stage.getWidth());
+        });
+        stage.widthProperty().addListener(stageSizeListener);
+        stage.heightProperty().addListener(stageSizeListener);
+    }
+
+    private Map<String, Label> createCryptoPriceLabels() {
+        Label bitcoinPrice = new Label("0");
+        bitcoinPrice.setId("BTC");
+
+        Label etherPrice = new Label("0");
+        etherPrice.setId("ETH");
+
+        Label liteCoinPrice = new Label("0");
+        liteCoinPrice.setId("LTC");
+
+        Label bitcoinCashPrice = new Label("0");
+        bitcoinCashPrice.setId("BCH");
+
+        Label ripplePrice = new Label("0");
+        ripplePrice.setId("XRP");
+
+        Map<String, Label> cryptoLabelsMap = new HashMap<>();
+        cryptoLabelsMap.put("BTC", bitcoinPrice);
+        cryptoLabelsMap.put("ETH", etherPrice);
+        cryptoLabelsMap.put("LTC", liteCoinPrice);
+        cryptoLabelsMap.put("BCH", bitcoinCashPrice);
+        cryptoLabelsMap.put("XRP", ripplePrice);
+
+        return cryptoLabelsMap;
+    }
+
+    private GridPane createGrid() {
+        GridPane grid = new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setAlignment(Pos.CENTER);
+        return grid;
+    }
+
+    private void addLabelsToGrid(Map<String, Label> labels, GridPane grid) {
+        int row = 0;
+        for (Map.Entry<String, Label> entry : labels.entrySet()) {
+            String cryptoName = entry.getKey();
+            Label nameLabel = new Label(cryptoName);
+            nameLabel.setTextFill(Color.BLUE);
+            nameLabel.setOnMousePressed(event -> nameLabel.setTextFill(Color.RED));
+            nameLabel.setOnMouseReleased((EventHandler) event -> nameLabel.setTextFill(Color.BLUE));
+
+            grid.add(nameLabel, 0, row);
+            grid.add(entry.getValue(), 1, row);
+
+            row++;
+        }
+    }
+
+    private Rectangle createBackgroundRectangleWithAnimation(double width, double height) {
+        Rectangle backround = new Rectangle(width, height);
+        FillTransition fillTransition = new FillTransition(Duration.millis(1000), backround, Color.LIGHTGREEN, Color.LIGHTBLUE);
+        fillTransition.setCycleCount(Timeline.INDEFINITE);
+        fillTransition.setAutoReverse(true);
+        fillTransition.play();
+        return backround;
+    }
+
+    @Override
+    public void stop() {
+        System.exit(0);
+    }
 
     public static class PricesContainer {
         private Lock lockObject = new ReentrantLock();
