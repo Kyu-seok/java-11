e567d4b5996a000b673fd697e354a617b92facf5
Kyu-seok <yeumkyuseok@gmail.com>
2022-07-26 17:02:22 +0800

    Implement MatricesReaderProducer class in MainApplication.java

--- Java_In_Depth/Multithreading-Concurrency-and-Performance-Optimization/Thread-Creation/multithreading/src/thread/semaphore/MainApplication.java
+++ Java_In_Depth/Multithreading-Concurrency-and-Performance-Optimization/Thread-Creation/multithreading/src/thread/semaphore/MainApplication.java
@@ -1,15 +1,62 @@
 package thread.semaphore;
 
+import java.io.FileReader;
 import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class MainApplication {
+
+    private static final int N = 10;
 
     public static void main(String[] args) {
 
     }
 
+    private static class MatricesReaderProducer extends Thread {
+        private Scanner scanner;
+        private ThreadSafeQueue queue;
+
+        public MatricesReaderProducer(FileReader reader, ThreadSafeQueue queue) {
+            this.scanner = new Scanner(reader);
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                float[][] matrix1 = readMatrix();
+                float[][] matrix2 = readMatrix();
+                if (matrix1 == null || matrix2 == null) {
+                    queue.terminate();
+                    System.out.println("No more matrices to read. Producer Thread is terminating");
+                    return;
+                }
+
+                MatricesPair matricesPair = new MatricesPair();
+                matricesPair.matrix1 = matrix1;
+                matricesPair.matrix2 = matrix2;
+
+                queue.add(matricesPair);
+            }
+        }
+
+        private float[][] readMatrix() {
+            float[][] matrix = new float[N][N];
+            for (int r = 0; r < N; r++) {
+                if (!scanner.hasNext()) {
+                    return null;
+                }
+                String[] line = scanner.nextLine().split(",");
+                for (int c = 0; c < N; c++) {
+                    matrix[r][c] = Float.valueOf(line[c]);
+                }
+            }
+            scanner.nextLine();
+            return matrix;
+        }
+    }
 
     private static class ThreadSafeQueue {
         private Queue<MatricesPair> queue = new LinkedList<>();
